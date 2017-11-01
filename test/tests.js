8a9d784ad30ddedcf33f2d98b9751eb1c02c50e9
Caelin Sheahan <caelinsheahan@gmail.com>
2017-11-01 15:03:34 -0600

    added new function to test

--- test/tests.js
+++ test/tests.js
@@ -2,13 +2,13 @@ const expect = chai.expect
 
 describe('playernames', function() {
     it('is an array filled with objects', function() {
-      expect(playernames).to.be.an('array').that.does.not.include('number').to.have.property('object');
+      expect(playernames).to.be.an('array')
     });
     it('contains only strings', function() {
       expect(playernames).does.not.include('number')
     });
     it('is filled with players', function() {
-      expect(playernames[22]).to.be.an('object');
+      expect(playernames[160]).to.be.an('object');
     });
 });
 
@@ -17,12 +17,33 @@ describe('name', function() {
       expect(name).to.be.a('function')
     });
     it('returns an array location', function() {
-      expect(name('caelin')).to.deep.equal([44])
+      expect(name('caelin')).to.be.an('object')
     });
 });
 
 describe('storeaccess', function() {
     it('is an array with no numbers', function() {
       expect(storeaccess).to.be.an('array').that.does.not.include('number')
+    });
+});
+describe('parsePersonPlayer', function() {
+    it('take a person object and return an object of strings', function() {
+      let person = {
+        "firstName": "DeMar",
+        "lastName": "DeRozan",
+        "scrubBenchStarter": "good",
+        "primarySkill": "dunking"
+      }
+      expect(parsePersonPlayer(person)).to.deep.equal({
+          "playerId": "DeMar+DeRozan",
+          "playerNewsID": "DeMarDeRozan",
+          "playerskill": "dunking",
+          "isplayergood": "good",
+          "namestorage": [
+            "DeMar",
+            "DeRozan"
+          ]
+        }
+      )
     });
 });
